09461f56b68aa74363fd444709b0181428a4bde5
Mahpara <mah4.para@gmail.com>
2025-12-08 16:35:27 +0000

    feat: navbar, logo and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import "./App.css";
+import NavBar from  "./Components/Navbar/Navbar.jsx";
 import Footer from  "./Components/Footer/Footer.jsx";
 
 export default function app (){
   return(
   <>
+  <NavBar />
   <Footer />
   </>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import "./Navbar.css";
 
 export default function Navbar (){
-  return(
-  <>
-  </>
-  );
+    return(
+    <nav className="navbar">
+        <div className="logo">
+        <img src="/logo.png" alt="PulseUI Mini Logo" className="logoImg" />
+        <span>PulseUI Mini</span>
+        </div>
+
+    <ul className="navLinks">
+        <li><a href="#branding">Branding</a></li>
+        <li><a href="#colors">Color Palette</a></li>
+        <li><a href="#typography">Typography</a></li>
+        <li><a href="#components">Components</a></li>
+        <li><a href="#glassmophorism">Glassmophorism</a></li>
+        <li><a href="#themes">Multi-Themes</a></li>
+
+    </ul>
+    </nav>
+    );
 }
